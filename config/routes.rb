2923df53ef97b921c91c0d9da4dd82f30deed9b0
daniiielak <danielaelisabeth.kepper@gmail.com>
2018-03-23 23:27:23 +0000

    added html, css, about, contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'site/about'
+
+  get 'site/contact'
+
+  get '/about' => 'site#about'
+  
+  get '/contact' => 'site#contact'
+
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'items#index'
+  
 end
