77e15d4e9ff46ac0c078b0e6a161d2ae360d3c07
daniiielak <danielaelisabeth.kepper@gmail.com>
2018-03-24 00:32:01 +0000

    added shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cart/index'
+
   get 'site/about'
   get 'site/contact'
 
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   
   get '/Admin' => 'user#admin_login'
   get '/logout' => 'user#logout'
+  
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
 
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
